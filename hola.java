76a36de75bbfc6bbd363dd2319a6c926ef609e2c
Darwin Uzcategui Portati Linux <darwin.uzcategui1973@gmail.com>
2022-11-26 11:54:29 -0400

    hola java version

---
+++ hola.java
@@ -0,0 +1,15 @@
+public class hola {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //String version = System.getProperty("java.version");
+        //String version = Runtime.class.getPackage().getImplementationVersion();
+        double version = Double.parseDouble(System.getProperty("java.specification.version"));
+		
+		System.out.println("hola Darwin.. java="+version);
+		
+
+	}
+
+}
